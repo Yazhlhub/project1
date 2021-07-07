2815ffb06425e8357355805932bfe9f903b46865
Yazhlhub <yazhini23042000@gmail.com>
2021-07-07 13:40:50 +0530

    First commit

---
+++ proj1/proj1/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proj1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
